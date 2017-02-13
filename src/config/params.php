00abb6ac3bcd161aaa61d23df04e1cb3bee41296
Muhammad Fauzil Haqqi <me@haqqi.net>
2017-02-13 19:25:14 +0700

    [add] default params for app. will be overriden by apps params

---
+++ src/config/params.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'chopper' => [
+        'footer' => '&copy; 2017 - Yii2 Chopper Admin Template by ' . '<a href="http://blog.haqqi.net" target="_blank">Haqqi</a> and Team',
+        
+    ]
+];
