29d06f3646f1e65197db986e97886a2362349286
MF Haqqi <me@haqqi.net>
2017-02-19 18:55:12 +0700

    [partly-done] Panel widget

--- src/views/feature/panel.php
+++ src/views/feature/panel.php
@@ -1,8 +1,31 @@
 <?php
 
+use haqqi\chopper\widgets\Panel;
+use rmrevin\yii\fontawesome\FA;
 
 ?>
 
-Ini panel
+<div class="row">
+    <div class="col-sm-6">
+        <?php
+        Panel::begin([
+            'icon' => FA::icon('clock-o'),
+            'title' => 'Standard Panel',
+            'description' => 'This is rendered via begin and end',
+            'footer' => 'Footer panel is rendered here.'
+        ]);
+        ?>
+        
+        <p>Panel contents / body can be added between <code>Panel::begin();</code> and <code>Panel::end();</code>. Please take a look at the source code of this page to understand more. You can also pass <code>body</code> parameter if using <code>Panel::widget();</code>.</p>
+        
+        <?php
+        Panel::end();
+        ?>
+    </div>
+    
+    <div class="col-sm-6">
+        
+    </div>
+</div>
 
 <?php
---
+++ src/widgets/Panel.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace haqqi\chopper\widgets;
+
+use yii\base\Widget;
+use yii\helpers\Html;
+
+class Panel extends Widget
+{
+    /** @var string $style Panel style. You can choose from default|primary|danger|info|warning|success */
+    public $style = 'default';
+
+    /** @var string $color Color style that override the style. If it is empty string, it will not be rendered. */
+    public $color = '';
+
+    /** @var string $icon Icon of the panel, before title. */
+    public $icon;
+
+    /** @var string $title Panel title. */
+    public $title;
+
+    /** @var string $description Panel description. Rendered after title. */
+    public $description;
+
+    /** @var string $body Panel body. */
+    public $body;
+
+    /** @var string $footer Panel footer. */
+    public $footer;
+
+    public function init()
+    {
+        parent::init();
+
+        // open panel tag
+        echo Html::beginTag('section', ['class' => 'panel panel-' . $this->style]);
+
+        $this->_renderHeader();
+
+        // open body tag
+        echo Html::beginTag('div', ['class' => 'panel-body']);
+    }
+
+    public function run()
+    {
+        if (!empty($this->body)) {
+            echo $this->body;
+        }
+
+        // close body tag
+        echo Html::endTag('div');
+
+        $this->_renderFooter();
+
+        // close panel tag
+        echo Html::endTag('section');
+    }
+
+    private function _renderHeader()
+    {
+        // if there is any content in the heading area
+        if (true) {
+            // open header tag
+            echo Html::beginTag('header', ['class' => 'panel-heading']);
+
+            $this->_renderTitle();
+
+            // close header tag
+            echo Html::endTag('header');
+        }
+    }
+
+    private function _renderFooter()
+    {
+        if ($this->footer === null) {
+            return;
+        }
+
+        echo Html::tag('footer', $this->footer, ['class' => 'panel-footer']);
+    }
+
+    private function _renderTitle()
+    {
+        // if there is no title, then no use to continue
+        if ($this->title === null) {
+            return;
+        }
+
+        // open title tag
+        echo Html::beginTag('div', ['class' => 'panel-title']);
+
+        // printing the title and icon
+        echo Html::beginTag('h3');
+        if ($this->icon !== null) {
+            echo $this->icon . ' ';
+        }
+        echo $this->title;
+        echo Html::endTag('h3');
+
+        // print the description
+        if ($this->description !== null) {
+            echo Html::tag('span', $this->description, ['class' => 'description']);
+        }
+
+        // close title tag
+        echo Html::endTag('div');
+    }
+}
