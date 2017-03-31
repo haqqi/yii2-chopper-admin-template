60c84fe5169f84cb100d6f64af29fc37eb1138b2
Haqqi <me@haqqi.net>
2017-04-01 02:42:54 +0700

    [change] set beforeAction in ChopperController to use specified asset

--- src/base/ChopperController.php
+++ src/base/ChopperController.php
@@ -2,9 +2,17 @@
 
 namespace haqqi\chopper\base;
 
+use haqqi\chopper\assets\ChopperAsset;
 use yii\web\Controller;
 
 class ChopperController extends Controller
 {
     public $layout = '@haqqi/chopper/views/layouts/default';
+
+    public function beforeAction($action)
+    {
+        \Yii::$app->chopper->assetBundleClass = ChopperAsset::className();
+
+        return parent::beforeAction($action);
+    }
 }
