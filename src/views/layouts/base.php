1c46d891b301dfb442f0d7c6762eef580a362b1e
Haqqi <me@haqqi.net>
2017-04-01 02:41:54 +0700

    [create] base layout

---
+++ src/views/layouts/base.php
@@ -0,0 +1,48 @@
+<?php
+
+/** @var \yii\web\View $this */
+
+use haqqi\chopper\Chopper;
+use yii\helpers\Html;
+use yii\helpers\Url;
+
+Chopper::getComponent()->registerThemeAsset($this);
+
+$this->beginPage();
+
+?>
+
+    <!DOCTYPE html>
+    <html lang="<?= Yii::$app->language; ?>">
+    <head>
+        <meta charset="<?php echo Yii::$app->charset; ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta http-equiv="content-type" content="text/html;charset=UTF-8">
+
+        <!-- icon -->
+        <?php echo Html::csrfMetaTags(); ?>
+        <title><?php echo Html::encode($this->title); ?></title>
+
+        <!-- js helper -->
+        <script type="text/javascript">
+          var BASE_URL = '<?= Url::base(true); ?>';
+        </script>
+
+        <!-- SEO things -->
+        <?php $this->head(); ?>
+    </head>
+    <body>
+    <?php $this->beginBody(); ?>
+
+    <div id="wrapper">
+        <?php echo $content; ?>
+    </div>
+
+    <?php $this->endBody(); ?>
+    </body>
+    </html>
+
+<?php
+
+$this->endPage();
